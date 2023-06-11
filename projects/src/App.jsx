1ed56c8658aa2cb75d081ab5164b3286fed0f05c
thandinhjs <dinhcongthan03@gmail.com>
2023-06-11 23:45:55 +0700

    first commit

---
+++ projects/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/layout-client/home"
+import Signin from "./pages/users/Signin"
+import Signup from "./pages/users/Signup"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* client */}
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<HomePage />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
