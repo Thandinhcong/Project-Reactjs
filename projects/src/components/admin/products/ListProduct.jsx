79c306560e1f4b124df95e87727a1bcb68fcdd9b
thandinhjs <dinhcongthan03@gmail.com>
2023-06-17 13:38:39 +0700

    update :paginnate

--- projects/src/components/admin/products/ListProduct.jsx
+++ projects/src/components/admin/products/ListProduct.jsx
@@ -1,31 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { getAllProduct } from '../../../instances/products'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAllProduct } from '../../../instances/products';
 
 const ListProduct = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1); // Trang hiện tại
+    const [itemsPerPage] = useState(4);
+
     const ListProduct = async () => {
         const { data } = await getAllProduct();
-        setProducts(data.products)
-        console.log(data);
-        console.log(products);
-    }
-    useEffect(() => {
-        ListProduct()
-    }, [])
-    return (
-        <div className='col-9 border ' >
-            <h3 className='text-center'>Danh sách sản phẩm</h3>
-            <div className='d-flex justify-content-between'>
-                <select name="" id="" className='form-select' style={{ width: '300px' }}>
-                    <option value="" className='form-option'>Lọc sản phẩm theo giá</option>
-                </select>
-                <Link to="/admin/them-san-pham" className='btn btn-outline-primary'>Thêm sản phẩm</Link>
+        setProducts(data.products);
+    };
 
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(products.length / itemsPerPage);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    useEffect(() => {
+        ListProduct();
+    }, []);
+
+    return (
+        <div className="col-9 border ">
+            <h3 className="text-center">Danh sách sản phẩm</h3>
+            <div className="d-flex justify-content-between">
+                <select className="form-select" style={{ width: '300px' }}>
+                    <option value="" className="form-option">
+                        Lọc sản phẩm theo giá
+                    </option>
+                </select>
+                <Link to="/admin/them-san-pham" className="btn btn-outline-primary">
+                    Thêm sản phẩm
+                </Link>
             </div>
-            <div className="table-responsive  mt-3">
-                <table className="table table-bordered ">
-                    <thead>
+            <div className='mt-3'>
+                <table className='table table-bordered'>
+                    <thead >
                         <tr>
                             <th>#</th>
                             <th>Tên sản phẩm</th>
@@ -40,8 +56,8 @@ const ListProduct = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {products.map((item, index) => {
-                            return <tr key={item._id} >
+                        {currentItems.map((item, index) => (
+                            <tr key={item._id}>
                                 <td>{index + 1}</td>
                                 <td>{item.name}</td>
                                 <td><img src={item.image} width={50} /></td>
@@ -52,16 +68,28 @@ const ListProduct = () => {
                                 <td className=''>{item.salient_features}</td>
                                 <td>
                                     <Link to="/admin/cap-nhat-san-pham" className=' me-2'><img width="15" height="15" src="https://img.icons8.com/ios/50/edit--v1.png" alt="edit--v1" /></Link>
-                                    <button className='border-0 bg-white'><span class="material-symbols-outlined">delete</span></button>
+                                    <button className='border-0 bg-white'><span className="material-symbols-outlined">delete</span></button>
                                 </td>
                             </tr>
-                        })}
+                        ))}
                     </tbody>
                 </table>
+
+                {/* Hiển thị các nút phân trang */}
+                <div>
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button
+                            className="btn btn-primary"
+                            key={index + 1}
+                            onClick={() => paginate(index + 1)}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default ListProduct
+export default ListProduct;
