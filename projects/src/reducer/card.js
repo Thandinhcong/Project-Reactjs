b598f301b3c195962e90aba142c06519440d76ba
thandinhjs <dinhcongthan03@gmail.com>
2023-09-04 21:37:39 +0700

    fix  bug không add được thêm sản phầm

--- projects/src/reducer/card.js
+++ projects/src/reducer/card.js
@@ -9,23 +9,23 @@ const cardReducer = (state = initialState, action) => {
             case "card/add":
                 const product = action.payload;
                 // tìm index trong giỏ hàng 
-                const exitsProductIndex = draftState.items.findIndex(item => item.id === product.id);
+                const exitsProductIndex = draftState.items.findIndex(item => item._id === action.payload._id);
                 // nếu không có sản phẩm nào trong giỏ hàng thì thêm nó vào
                 if (exitsProductIndex === -1) {
                     draftState.items.push(product)
 
                 } else {
                     // ngược lại thì cập nhật số lượng , dựa theo index mới tìm đuọc
-                    draftState.items(exitsProductIndex).quantity++
+                    draftState.items[exitsProductIndex].quantity++
                 }
                 break
             case "card/increase": {
-                draftState.items.find((item) => item.id === action.payload).quantity++
+                draftState.items.find((item) => item._id === action.payload).quantity++
                 break;
             }
             case "card/decrease": {
                 // tìm sản phẩm theo object  vừa click
-                const foundProduct = draftState.items.find((item) => item.id = action.payload)
+                const foundProduct = draftState.items.find((item) => item._id = action.payload)
                 // nếu có sản phẩm trong giỏ hàng thì giảm số lượng
                 foundProduct.quantity--
                 // nếu số lượng giảm dưới 1 thì xóa
@@ -33,7 +33,7 @@ const cardReducer = (state = initialState, action) => {
                     const confilm = window.confirm("Bạn có chắc chắc muốn xóa không");
                     // nếu được chọn thì xóa
                     if (confilm) {
-                        draftState.items = draftState.items.filter((item) => item.id !== foundProduct.id)
+                        draftState.items = draftState.items.filter((item) => item._id !== foundProduct._id)
                     }
                     foundProduct.quantity = 1
                 }
